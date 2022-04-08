9c6aa7ad733d2c33e466d349c725c49271d4e7d2
Maksim <Aleksandrov>
2022-04-08 13:55:04 +1000

    work auction dump

--- scripts/auction.lua
+++ scripts/auction.lua
@@ -2,6 +2,7 @@ Global("gAuctionScanCurrPage", 0)
 Global("gAuctionScanStartTimestampS", 0)
 Global("gAuctionSearchWts", {})
 Global("gAuctionScanSearchRequest", {})
+Global("gAuctionScanScannedLots", {})
 
 function OnChangeAuctionWidgetState( params )
     local sysName = common.GetAddonInfo()['sysFullName']
@@ -10,6 +11,27 @@ function OnChangeAuctionWidgetState( params )
     wtAuctionAddon:Show( wtAuction:IsVisibleEx() )
     LogInfo( "Auction widget state - ", wtAuction:IsVisibleEx() )
     gAuctionScanStartTimestampS = 0
+end
+
+function DumpScanResult()
+    local scanResult = {}
+    scanResult['request'] = gAuctionScanSearchRequest
+    scanResult['date'] = common.GetLocalDateTime()
+    scanResult['result'] = gAuctionScanScannedLots
+    userMods.SetGlobalConfigSection( "scan_dump", scanResult )
+end
+
+function ScanCurrentLots()
+    local auctions = auction.GetAuctions()
+    for k, lotId in pairs(auctions) do
+        local lotInfo = auction.GetAuctionInfo(lotId)
+        local itemInfo = itemLib.GetItemInfo(lotInfo.itemId)
+        local lotScanned = lotInfo
+        lotScanned['num'] = itemLib.GetStackInfo(lotInfo.itemId)['count']
+        lotScanned['name'] = itemLib.GetName(lotInfo.itemId)
+        lotScanned['timeLeft'] = nil
+        gAuctionScanScannedLots[tostring(lotInfo.id)] = lotScanned
+    end
 end
 
 function UpdateScanWidget( totalPages )
@@ -22,19 +44,29 @@ function UpdateScanWidget( totalPages )
         wtScanProgress:SetVal('actual_time', '0')
     else
         local d = GetTimestampS() - gAuctionScanStartTimestampS
-        wtScanProgress:SetVal('actual_time', tostring(d))
-
-        local auctions = auction.GetAuctions()
-        for i = 0, GetTableSize( auctions ) - 1 do
-            local auction = auction.GetAuctionInfo( auctions[ i ] )
-        end
+        wtScanProgress:SetVal('actual_time', string.format("%.1f", d))
     end
+end
+
+function SearchRequest()
+    gAuctionScanCurrPage = gAuctionScanCurrPage + 1
+    auction.Search( gAuctionScanSearchRequest, AUCTION_ORDERFIELD_LEFTTIME, true, gAuctionScanCurrPage )
 end
 
 function OnAuctionSearchResult( params )
 
-    UpdateScanWidget( params['totalPagesCount'] ) 
+    UpdateScanWidget( params['totalPagesCount'] )
 
+    if not (gAuctionScanStartTimestampS == 0) then
+        ScanCurrentLots()
+
+        if gAuctionScanCurrPage <= params['totalPagesCount'] then
+            SearchRequest()
+        else
+            DumpScanResult()
+            gAuctionScanStartTimestampS = 0
+        end
+    end
 end
 
 function GetSearchRequest()
@@ -65,8 +97,9 @@ end
 function OnAuctionScanStart( params )
     gAuctionScanStartTimestampS = GetTimestampS()
     gAuctionScanSearchRequest = GetSearchRequest()
-
-    auction.Search( gAuctionScanSearchRequest, AUCTION_ORDERFIELD_LEFTTIME, true, 1 )
+    gAuctionScanScannedLots = {}
+    gAuctionScanCurrPage = 0
+    SearchRequest()
 end
 
 function AuctionInit()
